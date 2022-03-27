1231cb8cb529163156daf697a53686214756a752
A-Apkhanov <apkhanov.a.s@gmail.com>
2022-03-27 14:07:11 +0300

    Add new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import {FC} from 'react';
+import { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import {MainPage} from './components/pages/MainPage'
+import { MainPage } from './components/pages/MainPage';
+import { AboutPage } from './components/pages/AboutPage';
+import { ContactPage } from './components/pages/ContanctPage';
+import { GamePage } from './components/pages/GamePage';
+import { Header } from './components/organisms/Header';
+import { Footer } from './components/organisms/Footer';
 
 export const App: FC = () => {
-	return (<MainPage/>);
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path='/' element={<MainPage />} />
+				<Route path='/home' element={<MainPage />} />
+				<Route path='/game' element={<GamePage />} />
+				<Route path='/about' element={<AboutPage />} />
+				<Route path='/contact' element={<ContactPage />} />
+			</Routes>
+			<Footer />
+		</>
+	);
 };
 
 export default App;
